7970a893f85821f78eb9bfe5311c6074d0c2da3a
yoondaebae <yoondaebae@gmail.com>
2024-05-05 17:53:23 +0900

    시작 알림 메시지 출력

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -4,8 +4,9 @@ package com.sbs.exam.board;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("Hello and welcome!");
-    }
+    System.out.println("== 자바 텍스트 게시판 0.1v ==");
+    System.out.println("== 자바 텍스트 게시판 시작 ==");
+    System.out.println("명령)");
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
+  }
 }
