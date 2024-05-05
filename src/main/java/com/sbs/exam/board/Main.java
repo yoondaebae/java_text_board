6a436bd232458d3d53d2280bcac59e4d291769fe
yoondaebae <yoondaebae@gmail.com>
2024-05-05 17:47:11 +0900

    수정

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -6,7 +6,6 @@ public class Main {
   public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+    System.out.println("Hello and welcome!");
     }
-  }
 }
